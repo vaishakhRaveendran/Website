5b84fbb6ef4aae382e01bce70dec756785d0b08f
vaishakhRaveendran <vaishakhraveendran2002@gmail.com>
2025-06-16 17:31:29 +0530

    Add About Page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
