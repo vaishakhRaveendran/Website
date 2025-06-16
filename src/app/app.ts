dfd03230006c26fecf5f862f0d2ed12e98c2253d
vaishakhRaveendran <vaishakhraveendran2002@gmail.com>
2025-06-16 21:35:30 +0530

    removed unnecessary imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { About } from './about/about';
 import { Footer } from './footer/footer';
-import { Projects } from './projects/projects';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [About,Footer,Projects,RouterOutlet],
+  imports: [Footer,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
