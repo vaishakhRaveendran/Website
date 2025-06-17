13af0adfc68c38d1c894af698a3b229dcf1095b5
vaishakhRaveendran <vaishakhraveendran2002@gmail.com>
2025-06-17 14:16:11 +0530

    Update

--- src/app/project-card/project-card.ts
+++ src/app/project-card/project-card.ts
@@ -1,5 +1,6 @@
 import { Component,input } from '@angular/core';
-import { IProject } from '../projects/projects';
+import { IProject,ProjectData } from '../project-data';
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-project-card',
   imports: [],
@@ -7,7 +8,15 @@ import { IProject } from '../projects/projects';
   styleUrl: './project-card.css'
 })
 export class ProjectCard {
-   project = input<IProject>();
+   project : IProject|undefined;
+   constructor(private projectData:ProjectData, private router: ActivatedRoute,){}
+   ngOnInit(){
+    const routeParam = parseInt(this.router.snapshot.paramMap.get('id')??'0');
+    if (routeParam) {
+      this.project = this.projectData.getProjectById(routeParam);
+      console.log(this.project?.description);
+   }}
+
    getProjectLinks(links: { [key: string]: string } | undefined) {
     if (!links) return [];
     return Object.entries(links).map(([key, value]) => ({
