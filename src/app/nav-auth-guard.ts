acbc53bdddd7fab9bb0d451c7fc8cf8cb994f2ea
vaishakhraveendran <vaishakhraveendran2002@gmail.com>
2026-09-10 16:52:23 +0530

    ui revamp

--- src/app/nav-auth-guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { AddProject } from './add-project/add-project';
-
-export const navAuthGuard: CanDeactivateFn<AddProject> = (component, currentRoute, currentState, nextState) => {
-  return component.checkUnSavedChanges();
-};
